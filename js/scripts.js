1a9ad0a82c10caf688b25a0e5a6f643ec47bf145
Robson Ramalhoso <robsonramalhoso91@gmail.com>
2023-06-04 21:27:32 +0100

    Portfolio v1.1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 function mydiscordid() {
     alert('My discord id " !nosbor_91#5638 "')
 }
+function sectionH2MouseOver() {
+    let sectionH2 = document.getElementById("sectionH2")
+    sectionH2.style.color = "#B97A4C"
+}
+function sectionH2MouseLeave() {
+    let sectionH2 = document.getElementById("sectionH2")
+    sectionH2.style.color = "#805333"
+}
